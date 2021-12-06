0d2cb3e5b03e4c4d29167f4c1046e82331e46269
gign <gign@mail.ru>
2021-12-07 04:25:34 +0900

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #мы связали view под именем post_list с корневым URL-адресом ''
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
